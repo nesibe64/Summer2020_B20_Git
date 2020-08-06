f998a6cd7d3f4294f641dd007d21f9ac0022369a
Nesibe64 <sabancinesibe@gmail.com>
2020-08-05 21:35:58 -0400

    add GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -4,6 +4,6 @@ public class GitFun {
     public static void main(String[] args) {
         System.out.println("benim adim Nesibe");
         //this line is from github hhaha
-        System.out.println("I am learning Java);
+        System.out.println("I am learning Java");
     }
 }
