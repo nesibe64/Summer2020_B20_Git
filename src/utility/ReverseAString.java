cde8991ba2d08a59302f31d84fd4fba81cb37f3e
Nesibe64 <sabancinesibe@gmail.com>
2020-08-05 21:00:27 -0400

    2020 Batch20

---
+++ src/utility/ReverseAString.java
@@ -0,0 +1,12 @@
+package utility;
+
+public class ReverseAString {
+
+    public static String reverse(String str){
+        String result = "";
+        for(int i = str.length()-1;  i >=0 ; i--){
+            result += str.charAt(i);
+        }
+        return result;
+    }
+}
