1861f9ddec425fa9d44fe5faa7c6b37ca4d277c9
nesibe64 <69082092+nesibe64@users.noreply.github.com>
2020-08-05 21:32:56 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,5 +3,7 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("benim adim Nesibe");
+        //this line is from github hhaha
+        System.out.println("I am learning Java);
     }
 }
